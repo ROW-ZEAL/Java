743298c88a13e1cf4c82806b358bb9a3d6ccd7b8
ROW-ZEAL <rojilnem.shrestha@gmail.com>
2023-11-23 22:02:13 +0545

    final output

--- src/main/java/com/example/javafxdemo/controller/DashboardController.java
+++ src/main/java/com/example/javafxdemo/controller/DashboardController.java
@@ -13,7 +13,6 @@ import javafx.scene.control.ListView;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 public class DashboardController {
